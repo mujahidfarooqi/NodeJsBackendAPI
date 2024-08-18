7e15d6fda778b671a5140be698da0fd838b81a51
mujahidfarooqi <mujahidfarooqi123@gmail.com>
2024-08-18 23:16:07 +0500

    delete unnecessary folders

--- signUp/routes.config.js
+++
@@ -1,9 +0,0 @@
-const controller = require('./controllers/signup.controller');
-exports.routesConfig = function (app) {
-    app.route('/api/signup').post( function (req, res) {
-        controller.signup(req, res, function (result) {
-            res.json(result);
-        });
-    });
-    
-};
